10756dddb8d50e2f4060fa2fbd36121460120aed
ismoil27 <abduraimovismoil98@gmail.com>
2024-04-10 22:31:51 +0900

    feat: publishing results page

--- classification/urls.py
+++ classification/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('images_result/', views.images_result, name='images_result'),
 ]
--- classification/views.py
+++ classification/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
-
-
 def home(request):
     return render(request, 'home.html')
+
+
+
+
+def images_result(request):
+    return render(request, 'images_result.html')
